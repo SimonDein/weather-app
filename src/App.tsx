c55a1b9f24fda66bf61559b0d6cfd6521521865c
Simon Dein <simon.dein@visma.com>
2023-06-15 17:51:01 +0200

    Mocked location data to start rendering a list of locations, and their weather, to the user

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { ConnectedWeatherListItem } from "./components/WheatherListItem.tsx";
+import { useCurrentCurrentCoordinates } from "./utils/useCurrentCoordinates.ts";
+import { Location } from "./types/types.ts";
+
 function App() {
+  const locations: Location[] = [
+    {
+      name: "Current location",
+      coordinates: useCurrentCurrentCoordinates(),
+      zipCode: undefined,
+    },
+    {
+      name: "Oslo",
+      coordinates: undefined,
+      zipCode: undefined,
+    },
+    {
+      name: "London",
+      coordinates: undefined,
+      zipCode: undefined,
+    },
+  ];
 
   return (
     <>
+      <ul>
+        {locations.map((location, index) => (
+          <ConnectedWeatherListItem location={location} key={index} />
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
