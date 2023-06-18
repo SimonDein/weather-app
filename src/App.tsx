f7ca3e39fef3c1fb596ceecebd06203417025ced
Simon Dein <simon.dein@visma.com>
2023-06-18 16:07:11 +0200

    user can click a weather location to enter a details screen. From the screen can go back to the dash board

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { ConnectedWeatherListItem } from "./components/WheatherListItem.tsx";
 import { useCurrentCurrentCoordinates } from "./utils/useCurrentCoordinates.ts";
 import { Location } from "./types/types.ts";
+import { useState } from "react";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState<
+    Location | undefined
+  >(undefined);
   const locations: Location[] = [
     {
       name: "Current location",
@@ -16,19 +21,72 @@ function App() {
     },
   ];
 
+  const currentPage =
+    selectedLocation === undefined ? (
+      <Dashboard
+        locations={locations}
+        onSelectLocation={(location) => setSelectedLocation(location)}
+      />
+    ) : (
+      <DetailsPage
+        location={selectedLocation}
+        onBack={() => setSelectedLocation(undefined)}
+      />
+    );
+
   return (
-    <div className="px-4 pt-4 bg-gray-950 text-white App">
+    <main className="px-4 pt-4 bg-gray-950 text-white App">{currentPage}</main>
+  );
+}
+
+export function DetailsPage({
+  location,
+  onBack,
+}: {
+  location: Location;
+  onBack: () => void;
+}) {
+  return (
+    <div>
+      <Button onClick={onBack}>
+        <IoIosArrowRoundBack size={40} />
+      </Button>
+      <span>Detail</span>
+      <div>
+        <span>{location.name}</span>
+      </div>
+    </div>
+  );
+}
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "ghost";
+}
+
+export function Button({ variant = "primary", ...rest }: ButtonProps) {
+  return <button {...rest}>{rest.children}</button>;
+}
+
+export function Dashboard({
+  locations,
+  onSelectLocation,
+}: {
+  locations: Location[];
+  onSelectLocation: (location: Location) => void;
+}) {
+  return (
+    <>
       <h1 className="text-xl">Dashboard</h1>
       <ul className="flex flex-col gap-2">
         {locations.map((location, index) => (
           <ConnectedWeatherListItem
-            onClick={() => console.log(locations[index])}
+            onClick={() => onSelectLocation(location)}
             location={location}
             key={index}
           />
         ))}
       </ul>
-    </div>
+    </>
   );
 }
 
