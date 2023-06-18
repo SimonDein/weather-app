233824af57c37bbd02d140c70e8137d17a11cd23
Simon Dein <simon.dein@visma.com>
2023-06-18 17:37:26 +0200

    Weather details page displays all details with a foundational layout according to the design

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,6 +1,11 @@
 import { Location } from "../types/types.ts";
 import { Button } from "../components/Button.tsx";
 import { IoIosArrowRoundBack } from "react-icons/io";
+import { PageTitle } from "../components/PageTitle.tsx";
+import {
+  temparatureFormatter,
+  useCurrentWeatherDataForLocation,
+} from "../components/WheatherListItem.tsx";
 
 export function DetailsPage({
   location,
@@ -9,15 +14,98 @@ export function DetailsPage({
   location: Location;
   onBack: () => void;
 }) {
+  const { data, isLoading, error } = useCurrentWeatherDataForLocation(location);
+  const visibilityInKM = new Intl.NumberFormat("en-EN", {
+    style: "unit",
+    unit: "kilometer",
+    minimumFractionDigits: 1,
+  });
+
+  if (error) {
+    return <div>{error.name}</div>;
+  }
+
+  if (!data) {
+    return null; // TODO: Handle missing data
+  }
+
+  // Get the local date and time components from the Date object
+  const formattedVisibilityKM = visibilityInKM.format(data.visibility / 1000);
   return (
-    <div>
-      <Button onClick={onBack}>
-        <IoIosArrowRoundBack size={40} />
-      </Button>
-      <span>Detail</span>
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
+        <Button className="flex items-center" onClick={onBack}>
+          <IoIosArrowRoundBack size={40} />
+        </Button>
+        <PageTitle>{location.name}</PageTitle>
+      </div>
       <div>
-        <span>{location.name}</span>
+        {!isLoading && (
+          <div className="flex flex-col items-center">
+            <span>{data?.weather[0].main}</span>
+            <span className="text-4xl">
+              {temparatureFormatter.format(data.main.temp)}
+            </span>
+
+            <div className="flex gap-4">
+              <span>L: {data?.main.temp_min}</span>
+              <span>H: {data?.main.temp_max}</span>
+            </div>
+
+            <div>
+              <div className="flex border-b w-full">
+                <WeatherMetaDataTile
+                  className="border-r flex-1"
+                  title="Sunrise"
+                  value={unixToLocaleTimeString(data.sys.sunrise)}
+                />
+                <WeatherMetaDataTile
+                  title="Sunset"
+                  value={unixToLocaleTimeString(data.sys.sunset)}
+                />
+              </div>
+              <div className="flex w-full">
+                <WeatherMetaDataTile
+                  title="Humidity"
+                  className="border-r flex-1"
+                  value={`${data.main.humidity}%`}
+                />
+                <WeatherMetaDataTile
+                  className="flex-1"
+                  title="Visibility"
+                  value={formattedVisibilityKM}
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
+}
+
+export function WeatherMetaDataTile({
+  title,
+  value,
+  className,
+}: {
+  title: string;
+  value: number | string;
+  className?: string;
+}) {
+  return (
+    <div className={`flex flex-col items-center flex-1 p-4 ${className}`}>
+      <span className="whitespace-nowrap">{title}</span>
+      <span className="whitespace-nowrap">{value}</span>
+    </div>
+  );
+}
+
+export function unixToLocaleTimeString(unixTime: number) {
+  const date = new Date(unixTime * 1000);
+
+  return date.toLocaleTimeString("en-EN", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
